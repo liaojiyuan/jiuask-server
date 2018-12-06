0aa4e50046f04673ec157aa51dc386839ef5fcbf
gaochaochao <gaochaochao@thunisoft.com>
2018-12-06 15:51:27 +0800

    提交

--- src/main/java/com/codedrinker/Application.java
+++ src/main/java/com/codedrinker/Application.java
@@ -1,6 +1,8 @@
 
 package com.codedrinker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,13 +19,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @ComponentScan
 @ImportResource({"classpath*:applicationContext.xml"})
 public class Application {
-
+	private static Logger logger = LoggerFactory.getLogger(Application.class);
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
 
     @RequestMapping("/")
     String index() {
+    	logger.info("进入首页后台处理");;
         return "index";
     }
 }
